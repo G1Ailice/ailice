aa811e6a98c8bc6cd37ce13da695c3985f3a9bb8
G1Ailice <groupailice@gmail.com>
2025-03-02 19:19:32 +0800

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ailice.online',
+    generateRobotsTxt: true, // (optional) generate a robots.txt file
+    // additional options can be added here
+  };
